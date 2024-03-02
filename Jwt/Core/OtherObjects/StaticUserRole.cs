d185365b5f23c5640c93db8f007df08eda69820e
Hasanboev Muhammadyusuf <hasanboevmuhammadyusuf@gmail.com>
2024-03-02 14:47:04 +0500

    Added Dtos and Roles

---
+++ Jwt/Core/OtherObjects/StaticUserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace Jwt.Core.OtherObjects
+{
+	public static class StaticUserRole
+	{
+		public const string Owner = "Owner";
+		public const string Admin = "Admin";
+		public const string User = "User";
+	}
+}
