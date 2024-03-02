01e299a16b75d4df8c8f1e3e5ae8c456946453b6
Hasanboev Muhammadyusuf <hasanboevmuhammadyusuf@gmail.com>
2024-03-02 14:31:43 +0500

    Implement Db and Identity

---
+++ Jwt/Core/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jwt.Core.Data
+{
+	public class AppDbContext : IdentityDbContext
+	{
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+
+
+}
